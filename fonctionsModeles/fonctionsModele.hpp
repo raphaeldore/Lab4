f93559ca37e2431426df7be04d8688f3757d1e41
Raphaël Doré <rdore@neomailbox.ch>
2015-03-03 14:36:07 -0500

    TrouverMax fonctionne maintenant avec des strings.
    Passation du dernier test de la fonction TrouverMax.

--- fonctionsModeles/fonctionsModele.hpp
+++ fonctionsModeles/fonctionsModele.hpp
@@ -1,7 +1,11 @@
+#include <string>
+
+using namespace  std;
+
 template<typename TYPE>
 TYPE TrouverMax(const TYPE _tableau[], const int& _nbElements)
 {
-	TYPE maxValueFound = 0;
+	TYPE maxValueFound = TYPE();
 	for (int i = 0; i < _nbElements; i++)
 	{
 		if (_tableau[i] > maxValueFound)
